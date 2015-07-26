4d76fbf775d20c419c308fcc245f8e8fb017b1ff
Sutirtha-Marjit <sutirtha.social.marjit@gmail.com>
2015-07-26 19:46:38 +0530

    Calendar widget & jQuery added

--- r/angular/p001/src/app/customutils/DatePicker.js
+++ r/angular/p001/src/app/customutils/DatePicker.js
@@ -2,44 +2,63 @@ function DatePicker(){
 	
 	var currentMonth,self = this;
 	self.today = new Date();
+	self.containerID = '#calendar-container-div';
 	self.days = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun'];
 	
 	currentMonth = self.today.getMonth();
 	
 	self.generateMonth = function(month,year){
 		var fullMonth=[],crDate=null;
+		
+		if(typeof month == 'undefined'){ month = new Date().getMonth(); }
+		if(typeof year == 'undefined'){ year = new Date().getFullYear(); }
+		
 		for(var i=1;i<=32;i++){	
 			crDate = new Date();
 			crDate.setFullYear(year);
 			crDate.setMonth(month);
 			crDate.setDate(i);
 			fullMonth.push(crDate);
-			console.log(crDate);
-	    }
+		}
 		return fullMonth;
 	};
 	
-	
-	self.generateCalHTML = function(container,month,year){
+	self.createContainerDiv = function(){
+		var div = $(self.containerID);
 		
-	   var i,m,fullMonth,theadHTML = '<thead><tr>',tbodyHTML='<tbody>';
-	   fullMonth = self.generateMonth(month,year);
-	   
+		if(div.length==0){
+			div = $('<div id="'+self.containerID.split('#')[1]+'"/>');
+		}
+		
+		return div;
+	}
+	
+	self.makeCalendar = function(month,year){		
+		var firstPos,containerDiv,fullMonth = self.generateMonth(month,year);
+		containerDiv = self.createContainerDiv();
+        containerDiv.html(self.generateEmptyCalHTML());
+		$('body').append(containerDiv);
+		firstPos = fullMonth[0].getDay();
+		console.log(containerDiv.find('td[data-index="'+firstPos+'"]'));
+		
+    };	
+	
+	self.generateEmptyCalHTML = function(){		
+	   var i,m,theadHTML = '<thead><tr>',tbodyHTML='<tbody>\n<tr>\n'; 	   
 	   for(i=0;i<self.days.length;i++){
-		theadHTML +='<th>'+<self.days[i]+'</th>';
+		theadHTML +='<th>'+self.days[i]+'</th>';
         }
 		theadHTML +='</thead></tr>';	
 		
-		for(m=1;m<=31;m++){
-			if(m%7==0){
-				tbodyHTML +='<tr>';
-			}
-		
+		for(m=1;m<=35;m++){
+			var endOf7days = (m%7==0);			
+			tbodyHTML += '<td data-index="'+m+'">'+m+'</td>\n';
+			if(endOf7days){tbodyHTML +='</tr>\n';}	
+			if(endOf7days){tbodyHTML +='<tr>\n';} 	
 		}
-	
+		
+		tbodyHTML += '</tr>\n</tbody>';
+	    return '<table class="table">'+theadHTML+tbodyHTML+'</table>';
 	}
-
-
-
 
 }
