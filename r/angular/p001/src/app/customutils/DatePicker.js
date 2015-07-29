6772155c6044d6e0b2c0dceeb38bf3b4b0985fe2
Sutirtha-Marjit <sutirtha.social.marjit@gmail.com>
2015-07-29 05:46:52 +0530

    DatePicker updated

--- r/angular/p001/src/app/customutils/DatePicker.js
+++ r/angular/p001/src/app/customutils/DatePicker.js
@@ -3,6 +3,7 @@ function DatePicker(){
 	var currentMonth,self = this;
 	self.today = new Date();
 	self.containerID = '#calendar-container-div';
+	self.maxCompartments = 42;
     self.months = ['Jan','Feb','Mar','Apr','May','June','Jul','Aug','Sept','Oct','Nov','Dec'];
 	self.days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
 	
@@ -28,10 +29,9 @@ function DatePicker(){
 		var div = $(self.containerID);
 		
 		if(div.length==0){
-			div = $('<div id="'+self.containerID.split('#')[1]+'"/>');
+			div = $('<div class="dateW" id="'+self.containerID.split('#')[1]+'"/>');
             div.html(self.generateEmptyCalHTML());
-		}
-		
+		}		
 		return div;
 	}
     
@@ -44,8 +44,24 @@ function DatePicker(){
         return newDate;
     };
 	
-	self.makeCalendar = function(month,year){		
-		var i,totalTDs=0,firstPos,containerDiv,tempDate=null,tempTitle='',fullMonth = self.generateMonth(month,year),dateSeris=0;
+	self.getDefaultDate = function(){
+		var d = new Date();
+		return { month:d.getMonth(), year:d.getFullYear()}	
+	};
+	
+	self.commonInvoke = function(){
+		$('*[data-prop="date"]')
+	
+	}
+	
+	self.makeCalendar = function(month,year){
+		var i,totalTDs=0,firstPos,containerDiv,tempDate=null,tempTitle='',fullMonth,dateSeris=0;
+		if(month==null || year==null){
+			var defaultDate = self.getDefaultDate();
+			month = defaultDate.month;
+			year = defaultDate.year;
+		}
+		fullMonth = self.generateMonth(month,year)
 		containerDiv = self.getContainerDiv();        
 		$('body').append(containerDiv);
 		firstPos = fullMonth[0].getDay();
@@ -55,12 +71,14 @@ function DatePicker(){
         containerDiv.find('td[data-index]').text('');
         totalTDs = containerDiv.find('td[data-index]').length;
         containerDiv.find('*[data-id="crPhase"]').text(self.months[month]+","+year);
-        while(i<35){
+        
+		while(i<self.maxCompartments){
           tempDate = fullMonth[dateSeris];
-		  
-		  if(tempDate.getMonth()== month){
-          tempTitle = tempDate.getDate()+"-"+self.months[tempDate.getMonth()]+"-"+year;
-		  containerDiv.find('td[data-index="'+i+'"]').html('<a data-toggle="tooltip" data-placement="top" class="btn btn-default btn-sm btn-block" title="'+tempTitle+'">'+tempDate.getDate()+'</a>');
+		  if(typeof tempDate!='undefined'){ 
+			  if(tempDate.getMonth()== month){
+			  tempTitle = self.days[tempDate.getDay()]+" "+tempDate.getDate()+" "+self.months[tempDate.getMonth()]+" "+year;
+			  containerDiv.find('td[data-index="'+i+'"]').html('<a data-toggle="tooltip" data-placement="top" class="btn btn-default btn-sm btn-block" title="'+tempTitle+'">'+tempDate.getDate()+'</a>');
+			  }
 		  }
           dateSeris++;
           i++;          
@@ -72,7 +90,7 @@ function DatePicker(){
     
     self.getHeaderDiv = function(){
                
-        return '<div><div class="btn-group" style="display:block; width:135px; margin:0 auto;"><a class="btn btn-success btn-sm"><span class="glyphicon glyphicon-chevron-left"></span></a><a data-id="crPhase" class="btn btn-sm btn-default">Jul 2014</a><a class="btn btn-success btn-sm"><span class="glyphicon glyphicon-chevron-right"></span></a></div></div>';
+        return '<div><div class="btn-group" style="display:block; width:200px; margin:0 auto;"><a class="btn btn-success btn-sm"><span class="glyphicon glyphicon-chevron-left"></span></a><a data-id="crPhase" class="btn btn-sm btn-default">Jul 2014</a><a class="btn btn-success btn-sm"><span class="glyphicon glyphicon-chevron-right"></span></a></div></div>';
     };
 	
 	self.generateEmptyCalHTML = function(){		
@@ -82,7 +100,7 @@ function DatePicker(){
         }
 		theadHTML +='</thead></tr>';	
 		
-		for(m=1;m<=35;m++){
+		for(m=1;m<=self.maxCompartments;m++){
 			var endOf7days = (m%7==0);			
 			tbodyHTML += '<td data-index="'+(m-1)+'">'+(m-1)+'</td>\n';
 			if(endOf7days){tbodyHTML +='</tr>\n';}	
